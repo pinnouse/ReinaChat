a58f0ba35977fa1019cc63835676b0e9e5708045
pinnouse <cupofpho@gmail.com>
2018-05-07 12:03:09 -0400

    Bot is now a class for better and easier use

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
 import bot
+import json
 app = Flask(__name__)
+
+chatbot = bot.Bot()
 
 @app.route("/api")
 def api():
-    seq, read = bot.sentence_to_seq(str(request.args.get('s')))
-    out = bot.decode_sequence(seq)
+    seq, read = chatbot.sentence_to_seq(str(request.args.get('s')))
+    out = chatbot.decode_sequence(seq)
     d = {
         'read_sentence': read,
         'out_sentence': out
@@ -15,10 +18,10 @@ def api():
 @app.route("/web")
 def web():
     print(request.args.get('s'))
-    sentence, _ = bot.sentence_to_seq(str(request.args.get('s')))
+    sentence, _ = chatbot.sentence_to_seq(str(request.args.get('s')))
     out = ""
-    if sentence is not None:
-        out = bot.decode_sequence(sentence)
+    if sentence is not None and sentence is not "":
+        out = chatbot.decode_sequence(sentence)
     return render_template('app.html', output=out)
 
 if __name__ == "__main__":

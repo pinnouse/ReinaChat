f98f91833648e34de5983296f545bc7a3e53b931
pinnouse <cupofpho@gmail.com>
2018-05-07 21:10:00 -0400

    Removal of class and 10 sentence seeds

--- flaskapp.py
+++ flaskapp.py
@@ -3,12 +3,10 @@ import json
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-chatbot = bot.Bot()
-
 @app.route("/api")
 def api():
-    seq, read = chatbot.sentence_to_seq(str(request.args.get('s')))
-    out = chatbot.decode_sequence(seq)
+    seq, read = bot.sentence_to_seq(str(request.args.get('s')))
+    out = bot.decode_sequence(seq)
     d = {
         'read_sentence': read,
         'out_sentence': out
@@ -17,10 +15,10 @@ def api():
 
 @app.route("/web")
 def web():
-    sentence, _ = chatbot.sentence_to_seq(str(request.args.get('s')))
+    sentence, _ = bot.sentence_to_seq(str(request.args.get('s')))
     out = ""
     if sentence is not None and sentence is not "":
-        out = chatbot.decode_sequence(sentence)
+        out = bot.decode_sequence(sentence)
         # print("output:", out)
     return render_template('app.html', output=out)
 

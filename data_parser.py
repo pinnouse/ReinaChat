140de9d59f8f5f2567bc23c052d463333aee2733
pinnouse <cupofpho@gmail.com>
2019-10-20 16:59:22 -0400

    Nicer configuration and hoepfully reads better

---
+++ data_parser.py
@@ -0,0 +1,14 @@
+# Used to clean up the training information
+# Usage: python data_parser.py <input training> <target training> <name of encoded output>
+import os.path as op
+import sys
+
+i = []
+o = []
+with open(op.join(op.dirname(__file__), sys.argv[1]), 'r') as f:
+    i = f.read().split('\n')
+with open(op.join(op.dirname(__file__), sys.argv[2]), 'r') as f:
+    o = f.read().split('\n')
+with open(op.join(op.dirname(__file__), sys.argv[3]), 'w') as f:
+    for ind in range(min(len(i), len(o))):
+        f.write(i[ind] + '+++$+++' + o[ind] + '\n')
